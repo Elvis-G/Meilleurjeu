e70932690f6192118da3392df7c1e26a7590d3fe
Elvis <59653433+GeorgesEtDavid@users.noreply.github.com>
2020-01-21 00:10:03 +0100

    Update travail.c

--- travail.c
+++ travail.c
@@ -3,6 +3,22 @@
 #include <windows.h>
 #include <time.h>
 
+void SetColor(int ForgC) {
+ WORD wColor;
+
+  HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+                       //We use csbi for the wAttributes word.
+ if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+ {
+                 //Mask out all but the background attribute, and add in the forgournd color
+      wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+      SetConsoleTextAttribute(hStdOut, wColor);
+ }
+ return;
+}
+
 
 typedef struct Monstre Monstre_t;
 struct Monstre
@@ -11,7 +27,7 @@ struct Monstre
     int attaque;
     int defense;
     int mp;
-    int vitesse;
+    int esquive;
 };
 
 typedef struct LeHeros LeHeros_t;
@@ -22,46 +38,106 @@ struct LeHeros
     int attaque;
     int defense;
     int mp;
-    int vitesse;
+    int esquive;
+};
+
+typedef struct Allies Allies_t;
+struct Allies
+{
+    int level;
+    int pointdevie;
+    int attaque;
+    int defense;
+    int mp;
+    int esquive;
 };
 
 int main (){
 
+int jeu = 0;
 int experience = 0;
 int skillpoints = 50;
+int coffre = 0;
+
+int avancer = 0;
 char* tableauMob[4];
 
+while (jeu == 0) {
+printf("%s\n\n", "");
+printf("%s\n", "                            _____________________________________________________________ ");
+printf("%s\n", "                           |                                                             |");
+printf("%s\n", "                           |       BIENVENUE CHER JOUEUR PREPARER VOUS A EMBARQUER       |");
+printf("%s\n", "                           |     DANS UNE AVENTURE OU SEUL LA MORT Y METTRA UN TERME,    |");
+printf("%s\n", "                           |                    ALORS BONNE CHANCE!!!                    |");
+printf("%s\n", "                           |_____________________________________________________________|");
 
+Sleep(4000);
+
+SetColor(3);
 LeHeros_t heros ={1, 250, 0, 0, 0, 0};
-printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< LE HEROS >>>>>>>>>>>>>>>>> ");
+printf("\n\n%s\n\n", "<<<<<<<<<<<<<<<<< VOUS >>>>>>>>>>>>>>>>> ");
 printf("%s%d\n", "Level : ", heros.level);
 printf("%s%d\n", "Point de vie : ", heros.pointdevie);
 printf("%s%d\n", "Attaque : ", heros.attaque);
 printf("%s%d\n", "Defense : ", heros.defense);
 printf("%s%d\n", "MP : ", heros.mp);
-printf("%s%d\n\n", "Vitesse : ", heros.vitesse);
+printf("%s%d\n\n", "Esquive : ", heros.esquive);
+SetColor(15);
 
-printf("\n%s\n", "Vous avez la possibilite d'attribuer au total 50 points de competences, choissisez intelligement");
-printf("%s%d", "Attaque : ", &heros.attaque);
+printf("\n%s\n", "Comme vous pouvez le constatez, vos stats se trouvent toutes a 0");
+printf("%s\n", "Vous allez donc devoir attribuer au total 50 points de competences, choissisez intelligement, il n'est pas possible de revenir en arriere");
+printf("%s", "Attaque : ");
+scanf("%d", &heros.attaque);
+coffre = skillpoints;
 skillpoints = skillpoints - heros.attaque;
+while (skillpoints < 0){
+  printf("\n%s", "Dis donc, vous avez utilisez plus de points de competences que vous en avez! Recommencez ");
+  printf("\n%s", "Attaque : ");
+  scanf("%d", &heros.attaque);
+  skillpoints = coffre - heros.attaque;
+}
 printf("\n%s%d", "Points de competences restant : ", skillpoints);
 
 printf("\n%s", "Defense : ");
 scanf("%d", &heros.defense);
+coffre = skillpoints;
 skillpoints = skillpoints - heros.defense;
+while (skillpoints < 0){
+  printf("\n%s", "Dis donc, vous avez utilisez plus de points de competences que vous en avez! Recommencez ");
+  printf("\n%s", "Defense : ");
+  scanf("%d", &heros.defense);
+  skillpoints = coffre - heros.defense;
+}
 printf("\n%s%d", "Points de competences restant : ", skillpoints);
 
 printf("\n%s", "MP : ");
 scanf("%d", &heros.mp);
+coffre = skillpoints;
 skillpoints = skillpoints - heros.mp;
-printf("\n%s", "Points de competences restant : ", skillpoints);
-
-printf("\n%s", "Vitesse : ");
-scanf("%d", &heros.vitesse);
-skillpoints = skillpoints - heros.vitesse;
+while (skillpoints < 0){
+  printf("\n%s", "Dis donc, vous avez utilisez plus de points de competences que vous en avez! Recommencez ");
+  printf("\n%s", "MP : ");
+  scanf("%d", &heros.mp);
+  skillpoints = coffre - heros.mp;
+}
 printf("\n%s%d", "Points de competences restant : ", skillpoints);
 
-printf("\n\n%s\n\n", "<<<<<<<<<<<<<<<<< LE HEROS >>>>>>>>>>>>>>>>> ");
+printf("\n%s", "Esquive : ");
+scanf("%d", &heros.esquive);
+coffre = skillpoints;
+skillpoints = skillpoints - heros.esquive;
+while (skillpoints < 0){
+  printf("\n%s", "Dis donc, vous avez utilisez plus de points de competences que vous en avez! Recommencez ");
+  printf("\n%s", "Esquive : ");
+  scanf("%d", &heros.esquive);
+  skillpoints = coffre - heros.esquive;
+}
+printf("\n%s%d", "Points de competences restant : ", skillpoints);
+
+printf("\n\n%s\n\n", "VOICI LA FICHE DE VOTRE PERSONNAGE ");
+
+SetColor(3);
+printf("\n\n%s\n\n", "<<<<<<<<<<<<<<<<< VOUS >>>>>>>>>>>>>>>>> ");
 printf("%s", "Level : ");
 printf("%d\n", heros.level);
 printf("%s", "Point de vie : ");
@@ -72,10 +148,125 @@ printf("%s", "Defense : ");
 printf("%d\n", heros.defense);
 printf("%s", "MP : ");
 printf("%d\n", heros.mp);
-printf("%s", "Vitesse : ");
-printf("%d\n\n", heros.vitesse);
+printf("%s", "Esquive : ");
+printf("%d\n\n", heros.esquive);
+SetColor(15);
+
+Sleep(2000);
+
+printf("%s\n", "                            _____________________________________________________________ ");
+printf("%s\n", "                           |                                                             |");
+printf("%s\n", "                           |       VOICI LES PERSONNAGES QUI VONT VOUS ACCOMPAGNER       |");
+printf("%s\n", "                           |_____________________________________________________________|");
+
+Sleep(2000);
+
+SetColor(4);
+Allies_t georges ={1, 150, 10, 5, 25, 10};
+printf("\n\n%s\n\n", "<<<<<<<<<<<<<<<<< GEORGES >>>>>>>>>>>>>>>>> ");
+printf("%s", "Level : ");
+printf("%d\n", georges.level);
+printf("%s", "Point de vie : ");
+printf("%d\n", georges.pointdevie);
+printf("%s", "Attaque : ");
+printf("%d\n", georges.attaque);
+printf("%s", "Defense : ");
+printf("%d\n", georges.defense);
+printf("%s", "MP : ");
+printf("%d\n", georges.mp);
+printf("%s", "Esquive : ");
+printf("%d\n\n", georges.esquive);
+SetColor(15);
+
+Sleep(1000);
+
+SetColor(14);
+Allies_t david ={1, 400, 5, 30, 10, 5};
+printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< DAVID >>>>>>>>>>>>>>>>> ");
+printf("%s", "Level : ");
+printf("%d\n", david.level);
+printf("%s", "Point de vie : ");
+printf("%d\n", david.pointdevie);
+printf("%s", "Attaque : ");
+printf("%d\n", david.attaque);
+printf("%s", "Defense : ");
+printf("%d\n", david.defense);
+printf("%s", "MP : ");
+printf("%d\n", david.mp);
+printf("%s", "Esquive : ");
+printf("%d\n\n", david.esquive);
+SetColor(15);
+
+Sleep(1000);
+
+SetColor(2);
+Allies_t bernard ={1, 180, 25, 5, 5, 10};
+printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< BERNARD >>>>>>>>>>>>>>>>> ");
+printf("%s", "Level : ");
+printf("%d\n", bernard.level);
+printf("%s", "Point de vie : ");
+printf("%d\n", bernard.pointdevie);
+printf("%s", "Attaque : ");
+printf("%d\n", bernard.attaque);
+printf("%s", "Defense : ");
+printf("%d\n", bernard.defense);
+printf("%s", "MP : ");
+printf("%d\n", bernard.mp);
+printf("%s", "Esquive : ");
+printf("%d\n\n", bernard.esquive);
+SetColor(15);
+
+Sleep(1000);
+
+SetColor(5);
+Allies_t daniel ={1, 300, 15, 15, 10, 10};
+printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< DANIEL >>>>>>>>>>>>>>>>> ");
+printf("%s", "Level : ");
+printf("%d\n", georges.level);
+printf("%s", "Point de vie : ");
+printf("%d\n", georges.pointdevie);
+printf("%s", "Attaque : ");
+printf("%d\n", georges.attaque);
+printf("%s", "Defense : ");
+printf("%d\n", georges.defense);
+printf("%s", "MP : ");
+printf("%d\n", georges.mp);
+printf("%s", "Esquive : ");
+printf("%d\n\n", georges.esquive);
+SetColor(15);
+
+Sleep(1000);
+
+printf("\n%s\n", "Avant de vous presentez leurs capacites, notez sur un papier les stats de vos personnages, cela peut s'averer utile");
+printf("\n%s\n", "Vous etes pret ? Si oui, entrer le numero -1- ");
+while (avancer != 1){
+scanf("%d", &avancer);
+}
+
+if (avancer == 1) {
+printf("%s\n", "                            _____________________________________________________________ ");
+printf("%s\n", "                           |                                                             |");
+printf("%s\n", "                           |     VOICI LES CAPACITES DETAILLES DE CHAQUE PERSONNAGES     |");
+printf("%s\n", "                           |_____________________________________________________________|");
+
+}
+
+tableauMob[0] = "bonjour";
+tableauMob[1] = "Bonsoir";
+tableauMob[2] = "Au revoir";
+tableauMob[3] = "Adieu";
+tableauMob[4] = "...";
+printf("%s %s %s %s %s", tableauMob[0], tableauMob[1], tableauMob[2], tableauMob[3], tableauMob[4]);
 
 
+
+Sleep(2000);
+system("cls");
+
+}
+return 0;
+
+SetColor(8);
 Monstre_t slime ={20, 5, 10, 0, 5};
 printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< SLIME >>>>>>>>>>>>>>>>> ");
 printf("%s", "Point de vie : ");
@@ -86,15 +277,8 @@ printf("%s", "Defense : ");
 printf("%d\n", slime.defense);
 printf("%s", "MP : ");
 printf("%d\n", slime.mp);
-printf("%s", "Vitesse : ");
-printf("%d\n", slime.vitesse);
+printf("%s", "Esquive : ");
+printf("%d\n", slime.esquive);
+SetColor(15);
 
-tableauMob[0] = "bonjour";
-tableauMob[1] = "Bonsoir";
-tableauMob[2] = "Au revoir";
-tableauMob[3] = "Adieu";
-tableauMob[4] = "...";
-printf("%s %s %s %s %s", tableauMob[0], tableauMob[1], tableauMob[2], tableauMob[3], tableauMob[4]);
-
-return 0;
 }
