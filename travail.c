c9a9c70434057d2ee418f587bb707c52c0218535
HugoPernel <59653428+HugoPernel@users.noreply.github.com>
2020-01-25 17:47:49 +0100

    EXP_LVL_MOBS

--- travail.c
+++ travail.c
@@ -25,7 +25,7 @@ typedef struct Monstre Monstre_t;
 struct Monstre
 {
     int level;
-    int pointdevie;
+    float pointdevie;
     int attaque;
     int defense;
     int mp;
@@ -35,24 +35,26 @@ struct Monstre
 typedef struct LeHeros LeHeros_t;
 struct LeHeros
 {
-    char* nom;
+
     int level;
-    int pointdevie;
+    float pointdevie;
     int attaque;
     int defense;
     int mp;
     int esquive;
+    int exp;
 };
 
 typedef struct Allies Allies_t;
 struct Allies
 {
     int level;
-    int pointdevie;
+    float pointdevie;
     int attaque;
     int defense;
     int mp;
     int esquive;
+    int exp;
 
     // Alchimiste , Necromancien, Paladin, Assassin
 };
@@ -91,19 +93,18 @@ printf("%s\n", "                           |____________________________________
 Sleep(4000);
 
 SetColor(3);
-<<<<<<< HEAD
-LeHeros_t heros = {1, 250, 0, 0, 0, 0};
-=======
-LeHeros_t heros = {"Inconnu", 1, 250, 0, 0, 0, 0};
->>>>>>> b85ae036d9ca7486101f155efd3e30d2e8ce9924
+
+LeHeros_t heros = {1, 250, 0, 0, 0, 0, 0};
+
+
 printf("\n\n%s\n\n", "<<<<<<<<<<<<<<<<< VOUS >>>>>>>>>>>>>>>>> ");
 printf("%s%d\n", "LEVEL : ", heros.level);
-printf("%s%d\n", "Classe : ", heros.nom);
 printf("%s%d\n", "Point de vie : ", heros.pointdevie);
 printf("%s%d\n", "Attaque : ", heros.attaque);
 printf("%s%d\n", "Defense : ", heros.defense);
 printf("%s%d\n", "MP : ", heros.mp);
 printf("%s%d\n", "Esquive : ", heros.esquive);
+printf("%s%d\n", "EXP : ", heros.exp);
 SetColor(15);
 
 printf("\n%s\n", "Comme vous pouvez le constatez, vos stats se trouvent toutes a 0");
@@ -172,6 +173,8 @@ printf("%s", "MP : ");
 printf("%d\n", heros.mp);
 printf("%s", "Esquive : ");
 printf("%d\n\n", heros.esquive);
+printf("%s", "EXP : ");
+printf("%d\n\n", heros.exp);
 SetColor(15);
 
 Sleep(2000);
@@ -184,7 +187,7 @@ printf("%s\n", "                           |____________________________________
 Sleep(2000);
 
 SetColor(4);
-Allies_t georges ={1, 150, 10, 5, 25, 10};
+Allies_t georges ={1, 150, 10, 5, 25, 10, 0};
 printf("\n\n%s\n\n", "<<<<<<<<<<<<<<<<< GEORGES >>>>>>>>>>>>>>>>> ");
 printf("%s", "LEVEL : ");
 printf("%d\n", georges.level);
@@ -198,12 +201,14 @@ printf("%s", "MP : ");
 printf("%d\n", georges.mp);
 printf("%s", "Esquive : ");
 printf("%d\n\n", georges.esquive);
+printf("%s", "EXP : ");
+printf("%d\n\n", georges.exp);
 SetColor(15);
 
 Sleep(1000);
 
 SetColor(14);
-Allies_t david ={1, 400, 5, 30, 10, 5};
+Allies_t david ={1, 400, 5, 30, 10, 5, 0};
 printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< DAVID >>>>>>>>>>>>>>>>> ");
 printf("%s", "LEVEL : ");
 printf("%d\n", david.level);
@@ -217,12 +222,14 @@ printf("%s", "MP : ");
 printf("%d\n", david.mp);
 printf("%s", "Esquive : ");
 printf("%d\n\n", david.esquive);
+printf("%s", "EXP : ");
+printf("%d\n\n", david.exp);
 SetColor(15);
 
 Sleep(1000);
 
 SetColor(2);
-Allies_t bernard ={1, 180, 25, 5, 5, 10};
+Allies_t bernard ={1, 180, 25, 5, 5, 10, 0};
 printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< BERNARD >>>>>>>>>>>>>>>>> ");
 printf("%s", "LEVEL : ");
 printf("%d\n", bernard.level);
@@ -236,12 +243,14 @@ printf("%s", "MP : ");
 printf("%d\n", bernard.mp);
 printf("%s", "Esquive : ");
 printf("%d\n\n", bernard.esquive);
+printf("%s", "EXP : ");
+printf("%d\n\n", bernard.exp);
 SetColor(15);
 
 Sleep(1000);
 
 SetColor(5);
-Allies_t daniel ={1, 300, 15, 15, 10, 10};
+Allies_t daniel ={1, 300, 15, 15, 10, 10, 0};
 printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< DANIEL >>>>>>>>>>>>>>>>> ");
 printf("%s", "LEVEL : ");
 printf("%d\n", georges.level);
@@ -255,6 +264,8 @@ printf("%s", "MP : ");
 printf("%d\n", georges.mp);
 printf("%s", "Esquive : ");
 printf("%d\n\n", georges.esquive);
+printf("%s", "EXP : ");
+printf("%d\n\n", georges.exp);
 SetColor(15);
 
 Sleep(1000);
@@ -316,9 +327,10 @@ printf("%s\n", "Tonerre [2]");
 printf("%s\n", "Renforcement [3]");
 SetColor(15);
 
+// Slime Base Stats //
 
 SetColor(8);
-Monstre_t slime ={1*progressionSlime, 20*slime.level, 5*slime.level, 10*slime.level, 1*slime.level, 5*slime.level};
+Monstre_t slime ={1, 150*slime.level, 5*slime.level, 10*slime.level, 5*slime.level, 5*slime.level};
 printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< SLIME >>>>>>>>>>>>>>>>> ");
 printf("%s", "LEVEL : ");
 printf("%d\n", slime.level);
@@ -332,8 +344,133 @@ printf("%s", "MP : ");
 printf("%d\n", slime.mp);
 printf("%s", "Esquive : ");
 printf("%d\n", slime.esquive);
-expSlime = 5*progressionSlime;
-experience = experience + expSlime;
+
+// Gobelin Base Stats //
+
+SetColor(4);
+Monstre_t gobelin ={1, 200*gobelin.level, 10*gobelin.level, 20*gobelin.level, 10*gobelin.level, 10*gobelin.level};
+printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< GOBELIN >>>>>>>>>>>>>>>>> ");
+printf("%s", "LEVEL : ");
+printf("%d\n", gobelin.level);
+printf("%s", "Point de vie : ");
+printf("%d\n", gobelin.pointdevie);
+printf("%s", "Attaque : ");
+printf("%d\n", gobelin.attaque);
+printf("%s", "Defense : ");
+printf("%d\n", gobelin.defense);
+printf("%s", "MP : ");
+printf("%d\n", gobelin.mp);
+printf("%s", "Esquive : ");
+printf("%d\n", gobelin.esquive);
+
+// Orgue Base Stats //
+
+SetColor(1);
+Monstre_t orque ={1, 300*orque.level, 15*orque.level, 25*orque.level, 15*orque.level, 15*orque.level};
+printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< ORQUE >>>>>>>>>>>>>>>>> ");
+printf("%s", "LEVEL : ");
+printf("%d\n", orque.level);
+printf("%s", "Point de vie : ");
+printf("%d\n", orque.pointdevie);
+printf("%s", "Attaque : ");
+printf("%d\n", orque.attaque);
+printf("%s", "Defense : ");
+printf("%d\n", orque.defense);
+printf("%s", "MP : ");
+printf("%d\n", orque.mp);
+printf("%s", "Esquive : ");
+printf("%d\n", orque.esquive);
+
+// Pheonix Base Stats //
+
+SetColor(11);
+Monstre_t pheonix ={1, 400*pheonix.level, 25*pheonix.level, 35*pheonix.level, 25*pheonix.level, 25*pheonix.level};
+printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< PHEONIX >>>>>>>>>>>>>>>>> ");
+printf("%s", "LEVEL : ");
+printf("%d\n", pheonix.level);
+printf("%s", "Point de vie : ");
+printf("%d\n", pheonix.pointdevie);
+printf("%s", "Attaque : ");
+printf("%d\n", pheonix.attaque);
+printf("%s", "Defense : ");
+printf("%d\n", pheonix.defense);
+printf("%s", "MP : ");
+printf("%d\n", pheonix.mp);
+printf("%s", "Esquive : ");
+printf("%d\n", pheonix.esquive);
+
+// Dragon Base Stats //
+
+SetColor(10);
+Monstre_t dragon ={1, 500*dragon.level, 50*dragon.level, 50*dragon.level, 50*dragon.level, 50*dragon.level};
+printf("\n%s\n\n", "<<<<<<<<<<<<<<<<< DRAGON >>>>>>>>>>>>>>>>> ");
+printf("%s", "LEVEL : ");
+printf("%d\n", dragon.level);
+printf("%s", "Point de vie : ");
+printf("%d\n", dragon.pointdevie);
+printf("%s", "Attaque : ");
+printf("%d\n", dragon.attaque);
+printf("%s", "Defense : ");
+printf("%d\n", dragon.defense);
+printf("%s", "MP : ");
+printf("%d\n", dragon.mp);
+printf("%s", "Esquive : ");
+printf("%d\n", dragon.esquive);
+
+// Gain d'expérience//
+
+if (slime.pointdevie <= 0)
+{
+  heros.exp = heros.exp + 1;
+  georges.exp = heros.exp + 1;
+  david.exp = heros.exp + 1;
+  bernard.exp = heros.exp + 1;
+  daniel.exp = heros.exp + 1;
+
+};
+
+// Monter de Level //
+if (heros.exp >= heros.exp + 5 && georges.exp >= georges.exp + 5 && daniel.exp >= daniel.exp + 5 && bernard.exp >= bernard.exp + 5 && david.exp >= david.exp + 5)
+{
+  heros.level = heros.level + 1;
+  georges.level = georges.level + 1;
+  david.level = david.level + 1;
+  bernard.level = bernard.level + 1;
+  daniel.level = daniel.level + 1;
+  slime.level = slime.level + 1;
+};
+
+// Monter des PV Slime si Level up//
+
+if (slime.level = slime.level + 1)
+{
+  slime.pointdevie = slime.pointdevie * 1.1;
+};
+// Monter des PV Heros si Level up//
+
+if (heros.level = heros.level + 1)
+{
+heros.pointdevie = heros.pointdevie * 1.1;
+};
+// Monter des PV Allies si Level up//
+
+if (georges.level = georges.level + 1)
+{
+georges.pointdevie = georges.pointdevie * 1.1;
+};
+if (david.level = david.level + 1)
+{
+david.pointdevie = david.pointdevie * 1.1;
+};
+if (daniel.level = daniel.level + 1)
+{
+daniel.pointdevie = daniel.pointdevie * 1.1;
+};
+if (bernard.level = bernard.level + 1)
+{
+bernard.pointdevie = bernard.pointdevie * 1.1;
+};
+
 SetColor(15);
 
 Sleep(2000);
